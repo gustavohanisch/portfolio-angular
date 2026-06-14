ff3176fecc91009445bbece79c40dc83493ea75e
gustavohanisch <20241ctb0100025@estudantes.ifpr.edu.br>
2026-06-14 17:24:47 +0000

    Aula15: Configura ambiente Angular com Material e primeira tela do Portfolio

--- portfolio-angular/src/app/contato/contato.ts
+++ portfolio-angular/src/app/contato/contato.ts
@@ -1,9 +1,22 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-contato',
-  imports: [],
+  imports: [
+    FormsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule
+  ],
   templateUrl: './contato.html',
-  styleUrl: './contato.css',
+  styleUrl: './contato.css'
 })
 export class Contato {}
